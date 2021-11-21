31e222433b39f5274656f6f13ec1e7ea3677f24a
shalda <zeppelin.ua@gmail.com>
2021-11-21 20:52:21 +0200

    gp initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,20 @@
 export const environment = {
-  firebase: {
-    projectId: 'fbauth-853b0',
-    appId: '1:636570909554:web:4bc1a5c01c9d40b59ffbeb',
-    databaseURL: 'https://fbauth-853b0-default-rtdb.firebaseio.com',
-    storageBucket: 'fbauth-853b0.appspot.com',
+  firebaseConfig: {
     apiKey: 'AIzaSyDBxWWupnR7AghW90xLVKdUFXAQ_YoQ9uI',
     authDomain: 'fbauth-853b0.firebaseapp.com',
+    databaseURL: 'https://fbauth-853b0-default-rtdb.firebaseio.com',
+    projectId: 'fbauth-853b0',
+    storageBucket: 'fbauth-853b0.appspot.com',
     messagingSenderId: '636570909554',
+    appId: '1:636570909554:web:4bc1a5c01c9d40b59ffbeb',
     measurementId: 'G-0WK2PH0LZF',
+  },
+  azureADConfig:{
+    clientId: '87e8dac9-510d-4444-992d-65cf33cdcf65'
+  },
+  awsConfig: {
+    cognitoUserPoolId: 'us-east-1_dE4Zy1ayW',
+    cognitoAppClientId: 'k9qtr0ehg2vvd904q04khb0u1'
   },
   production: true
 };
